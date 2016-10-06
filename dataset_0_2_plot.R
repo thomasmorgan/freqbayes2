c3878f91225b7d5d701d14ecb9272b5ec440aae5
Lotty Brand <LottyBrand@8afbb4f9.st-andrews.ac.uk>
2016-10-06 15:40:18 +0100

    wahooo! fixed! going to edit a bit to make it look nicer and do for no effect.

--- dataset_0_2_plot.R
+++ dataset_0_2_plot.R
@@ -7,29 +7,34 @@ library(ggplot2)
 saved_0_2 <- read.delim("saved_0_2.txt")
 
 plot_dataset_0_2 <- saved_0_2[,c("expt","analysis_type","b_sex_cond_lower","b_sex_cond_upper","b_sex_cond_med")]
-plot_dataset_0_2$analysis_type <- as.factor(plot_dataset_0_2$analysis_type)
-#plot_dataset_0_2$expt <- as.factor(plot_dataset_0_2$expt)
 
-#making subsets
 
+#transform everything to same scale as anovas
+plot_dataset_0_2$estimate <- ifelse((plot_dataset_0_2$analysis_type == "anova"), plot_dataset_0_2$b_sex_cond_med, (exp(plot_dataset_0_2$b_sex_cond_med)/(1+exp(plot_dataset_0_2$b_sex_cond_med)))-0.5)
+plot_dataset_0_2$estimate_upper <- ifelse((plot_dataset_0_2$analysis_type == "anova"), plot_dataset_0_2$b_sex_cond_upper, (exp(plot_dataset_0_2$b_sex_cond_upper)/(1+exp(plot_dataset_0_2$b_sex_cond_upper)))-0.5)
+plot_dataset_0_2$estimate_lower <- ifelse((plot_dataset_0_2$analysis_type == "anova"), plot_dataset_0_2$b_sex_cond_lower, (exp(plot_dataset_0_2$b_sex_cond_lower)/(1+exp(plot_dataset_0_2$b_sex_cond_lower)))-0.5)
+
+
+
+##making subsets - no longer needed##
 #just_anova <- subset(plot_dataset_0_2, plot_dataset_0_2$analysis_type=="anova")
 #just_glmm <- subset(plot_dataset_0_2, plot_dataset_0_2$analysis_type=="glmm")
 #just_bglmm <- subset(plot_dataset_0_2, plot_dataset_0_2$analysis_type=="bglmm")
 #just_pp <- subset(plot_dataset_0_2, plot_dataset_0_2$analysis_type =="pp")
 
-#does weird removing of rows
-limits <- aes(ymax = plot_dataset_0_2$b_sex_cond_upper, ymin = plot_dataset_0_2$b_sex_cond_lower)
-APlot <- ggplot(data = plot_dataset_0_2, aes(expt, b_sex_cond_med, colour = analysis_type))
+#anova on a different scale..? 
+limits <- aes(ymax = plot_dataset_0_2$estimate_upper, ymin = plot_dataset_0_2$estimate_lower)
+APlot <- ggplot(plot_dataset_0_2, aes(x=expt, y=estimate, group =analysis_type, col=analysis_type))
 APlot + geom_point(data = plot_dataset_0_2, size = 1.5) + 
   geom_errorbar(limits, width = 0.08) +
-  ylab("estimate") +
-  scale_y_continuous(limits=c(0,1), expand = c(0,0)) +
-  scale_x_continuous(limits=c(1,60)) 
+  ylab("estimate")
 
-#try adding up layers a la http://stackoverflow.com/questions/3777174/plotting-two-variables-as-lines-using-ggplot2-on-the-same-graph
- 
-limits <- aes(ymax = plot_dataset_0_2$b_sex_cond_upper, ymin = plot_dataset_0_2$b_sex_cond_lower)
-APlot <- ggplot(data = plot_dataset_0_2, aes(expt)) +
-  geom_point(aes(y= anova, colour="anova")) +
-  geom_point(aes(y= glmm, colour="glmm"))
-APlot  
+
+
+
+
+
+
+
+
+
